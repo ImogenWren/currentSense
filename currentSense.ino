8ad984b7c82ea75d3c9f901cd8210d97c0cb71e9
ImogenWren <imogen.wren91@gmail.com>
2022-03-29 14:44:44 +0100

    Update currentSense.ino

--- currentSense.ino
+++ currentSense.ino
@@ -29,15 +29,16 @@
 #include <avr/interrupt.h>
 
 
+
 #define BLINK_DELAY_MS 500
 
 void blink(uint8_t delayOneTime = 100, uint8_t delayTwoTime = 200) {
   //   PORTD = (1 << PD2); // Set PD2 High
   PORTD = (0 << PD3) | (1 << PD2); // Set PD3 Low
-  _delay_ms(1000);     // Delay
+  _delay_ms(500);     // Delay
   //    PORTD = (0 << PD2); // Set PD2 Low
   PORTD = (1 << PD3) | (0 << PD2); // Set PD3 Low
-  _delay_ms(500);     // Delay
+  _delay_ms(200);     // Delay
 }
 
 
